61201ba9cf7b59a2f1cf1c8debb97128c0c603de
Facu <f.taramasco@hotmail.com.ar>
2016-07-27 11:16:45 -0300

    Initial commit

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,27 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import modelo.Negocio;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controlador implements Initializable {
+
+    private Negocio modelo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        modelo = new Negocio();
+    }
+
+
+    @FXML
+    public void metodoPrueba(ActionEvent e) {
+        modelo.hacerUnaTarea();
+    }
+
+
+}
---
+++ src/modelo/Negocio.java
@@ -0,0 +1,20 @@
+package modelo;
+
+/**
+ * Created by martin on 27/07/16.
+ */
+public class Negocio {
+
+    public Negocio() {
+
+    }
+
+    public void hacerUnaTarea() {
+        System.out.println("realice una tarea X");
+    }
+
+    public boolean otroProcedimiento() {
+        return true;
+    }
+
+}
